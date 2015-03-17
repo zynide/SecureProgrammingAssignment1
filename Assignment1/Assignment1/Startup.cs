f74ebe9b9d60b58f9885e7aa72c8c36a92746ed4
Jesus Parra <j.parra@att.net>
2015-03-16 20:45:01 -0500

    Initial Commit
    This includes just the default Microsoft template for a blank solution.

---
+++ Assignment1/Assignment1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment1.Startup))]
+namespace Assignment1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
