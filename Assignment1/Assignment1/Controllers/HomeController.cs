2d06ae1ebc8df97d4d796bb440b7c5a4763291f1
Jesus Parra <j.parra@att.net>
2015-03-19 20:39:11 -0500

    got file to the server

--- Assignment1/Assignment1/Controllers/HomeController.cs
+++ Assignment1/Assignment1/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,5 +14,47 @@ namespace Assignment1.Controllers
         {
             return View();
         }
+
+
+        public ContentResult UploadFile(string test)
+        {
+            HttpRequestBase request = HttpContext.Request;
+            bool formUpload = request.Files.Count > 0;
+            string xFileName = request.Headers["X-File-Name"];
+            string qqFile = request["qqfile"];
+            string formFilename = formUpload ? request.Files[0].FileName : null;
+            string filename = xFileName ?? qqFile ?? formFilename;
+            Stream inputStream = formUpload ? request.Files[0].InputStream : request.InputStream;
+            string filePath = Path.Combine(HttpContext.Server.MapPath("~/App_Data"), filename);
+            string fileExtension = Path.GetExtension(filename);
+            BinaryReader binReader = new BinaryReader(inputStream);
+            byte[] binData = binReader.ReadBytes((int)inputStream.Length);
+            
+            string result = "{\"success\":true}";
+            return new ContentResult { Content = result, ContentType = "text/plain" };
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+                try
+                {
+                    string path = Path.Combine(Server.MapPath("~/Images"),
+                                               Path.GetFileName(file.FileName));
+                    ////file.SaveAs(path);
+                    ViewBag.Message = "File uploaded successfully";
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = "ERROR:" + ex.Message.ToString();
+                }
+            else
+            {
+                ViewBag.Message = "You have not specified a file.";
+            }
+            return View();
+        }
+
     }
 }
--- Assignment1/Assignment1/Scripts/home.js
+++ Assignment1/Assignment1/Scripts/home.js
@@ -1,8 +1,86 @@
 ﻿var HomeModel = function () {
     var self = this;
     self.file = ko.observable();
+    self.UploadFile = function () {
+        var files = document.getElementById('files').files;
+        var selectedFile = files[0];
+        var reader = new FileReader();
+        var fileString;
+        var testString = "test\n";
+        reader.readAsBinaryString(selectedFile);
+
+
+        reader.onloadend = function (evt) {
+            console.log("now executing onloadend");
+            //if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+            //    fileString = evt.target.result;
+            //    //console.log(fileString + " fileString");
+            //    //self.sendFileToServer(fileString);
+            //    console.log(testString);
+            //    console.log(fileString);
+            //    $.ajax({
+            //        url: '/Home/UploadFile',
+            //        type: 'POST',
+            //        data: { file: fileString },
+            //        dataType: 'json',
+            //        success: function (json) {
+            //            console.log("upload successful");
+            //        }
+            //    });
+            //}
+        };
+        self.sendFileToServer = function (fileRequired) {
+            console.log(fileRequired);
+            $.ajax({
+                url: '/Home/UploadFile',
+                type: 'POST',
+                data: { file: fileRequired },
+                dataType: 'json',
+                success: function (json) {
+                    console.log("upload successful");
+                }
+            });
+        };
+    };
+
+    self.CreateUploader = function (elementID, buttonText, imageCategory, imageIndex) {
+        var uploader = new qq.FineUploader({
+            element: document.getElementById(elementID),
+            request: {
+                endpoint: '/Home/UploadFile',
+                params: {
+                    'test': "test"
+                }
+            },
+            validation: {
+                acceptFiles: 'image/*, application/pdf',
+                allowedExtensions: ['pdf', 'tif', 'tiff', 'bmp', 'png', 'gif', 'jpeg', 'jpg'],
+                sizeLimit: 204800000 // 200 MB = 200,000 * 1024 bytes
+            },
+            text: {
+                uploadButton: buttonText
+            },
+            template: '<pre ><span>{dragZoneText}</span></pre>' +
+            '<input type="hidden" id="' + imageCategory + 'Input"></input>' +
+            '<input type="file"  data-input="false" data-icon="false" data-buttontext="Upload" id="filestyle-' + imageIndex + '" style="position: absolute; left: -9999px;">' +
+            '<div id="summaryUpload" >{uploadButtonText}</div>' +
+            '<ul  style="margin-top: 3px; margin-right: 10px; float: right; display:inline-block; text-align: center;"></ul>',
+            classes: {
+                retryable: 'false',
+                success: 'alert alert-success',
+                fail: 'alert alert-error'
+            },
+            callbacks: {
+                onSubmit: function (id, fileName, responseJSON) {
+                    console.log("this shit worked");
+                }
+            },
+            debug: true
+        });
+    };
 
 }
 
-// Activates knockout.js
-ko.applyBindings(new HomeModel());
+var homeModel = new HomeModel();
+
+ko.applyBindings(homeModel);
